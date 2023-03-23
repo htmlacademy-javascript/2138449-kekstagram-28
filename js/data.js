48b0f2467db54b75ba0e22ff2619d63744c37f7a
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-03-23 16:34:50 +0300

    восстанавливаю файлы

--- js/data.js
+++ js/data.js
@@ -1,5 +1,22 @@
 import { getRandomInteger, createRandomIdFromRangeGenerator } from './util.js';
 
+// Значения для ID комментариев
+const MIN_COMMENT_ID = 1;
+const MAX_COMMENT_ID = 100000;
+
+//Количество аватарок
+const AVATAR_COUNT = 6;
+
+//Длина комментариев
+const COMMENTS_LENGTH = 11;
+
+//Количество лайков
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
+
+// Количество объектов-описаний, которые хотим сгенерировать
+const DESCRIPTIONS_COUNT = 25;
+
 // Описания к фотографиям
 const DESCRIPTIONS = [
   'прекрасный вид',
@@ -31,34 +48,30 @@ const COMMENTS_TEXT = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-// Количество объектов-описаний, которые хотим сгенерировать
-const DESCRIPTIONS_COUNT = 25;
-
-const generatePhotoId = createRandomIdFromRangeGenerator(1, 25);
-const generateCommentId = createRandomIdFromRangeGenerator(1, 100000);
-
+const generateCommentId = createRandomIdFromRangeGenerator(MIN_COMMENT_ID, MAX_COMMENT_ID);
 
 // Функция для создания объектов comments
 function createComments () {
   return {
     id: generateCommentId(),
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg.`,
+    avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
     message: COMMENTS_TEXT[getRandomInteger(0, COMMENTS_TEXT.length - 1)],
     name: NAMES[getRandomInteger(0, NAMES.length - 1)],
   };
 }
 
 // Функция для создания описаний к фотографиям
-function createPhotoDescription () {
+function createPhotoDescription (index) {
   return {
-    id: generatePhotoId(),
-    url: `photos/${getRandomInteger(1, 25)}.jpg`,
+    id: index,
+    url: `photos/${ index }.jpg`,
     description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-    likes: getRandomInteger(15, 200),
-    comments: Array.from({length: 2}, createComments),
+    likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+    comments: Array.from({length: COMMENTS_LENGTH}, createComments),
   };
 }
 
 // Создаём массив из 25 объектов
-const createPhotoDescriptions = () => Array.from({length: DESCRIPTIONS_COUNT}, createPhotoDescription);
+const createPhotoDescriptions = () => Array.from({length: DESCRIPTIONS_COUNT}, (_, photoIndex) => createPhotoDescription(photoIndex + 1));
+
 export { createPhotoDescriptions };
