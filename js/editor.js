41302b0d74ee57e8785112e933dcfda3e88dac3a
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-03-26 19:00:18 +0300

    Завел файл для редактирования фото

---
+++ js/editor.js
@@ -0,0 +1,8 @@
+
+
+const SCALE_STEP = 25;
+const MAX_SCALE_COUNT = 100;
+const MIN_SCALE_COUNT = 25;
+
+const increaseInSizeButton = document.querySelector('.scale__control--bigger');
+const reductionSizeButton = document.querySelector('.scale__control--bigger');
