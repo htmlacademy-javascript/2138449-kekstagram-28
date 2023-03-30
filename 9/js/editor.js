71b52ec6de6d5bd60ccb514ddd5013251134498d
Keks <keks@htmlacademy.ru>
2023-03-30 05:57:33 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/editor.js
@@ -0,0 +1,48 @@
+const SCALE_STEP = 25;
+const MAX_SCALE_COUNT = 100;
+const MIN_SCALE_COUNT = 25;
+const DEFAULT_SCALE_VALUE = 100;
+
+const biggerSizeButton = document.querySelector('.scale__control--bigger');
+const smallerSizeButton = document.querySelector('.scale__control--smaller');
+const scaleElement = document.querySelector('.scale__control--value');
+const imageUploadPreview = document.querySelector('.img-upload__preview img');
+
+
+const resizeImg = (value) => {
+  imageUploadPreview.style.transform = `scale(${value / 100})`;
+  scaleElement.value = `${value}%`;
+};
+
+//Функция уменьшения размера фото
+const increaseButtonValue = () => {
+  const currentValue = parseInt(scaleElement.value, 10);
+  let newScaleValue = currentValue - SCALE_STEP;
+  if (newScaleValue < MIN_SCALE_COUNT) {
+    newScaleValue = MIN_SCALE_COUNT;
+  }
+
+  resizeImg(newScaleValue);
+};
+
+//Функция увелечения размера фото
+const decreaseButtonValue = () => {
+  const currentValue = parseInt(scaleElement.value, 10);
+  let newScaleValue = currentValue + SCALE_STEP;
+  if (newScaleValue > MAX_SCALE_COUNT) {
+    newScaleValue = MAX_SCALE_COUNT;
+  }
+
+  resizeImg(newScaleValue);
+};
+
+//Функция по сбросу значения шкалы до первоначального состояния
+const scaleValueReset = () => resizeImg(DEFAULT_SCALE_VALUE);
+
+//Вешаем слушатель на кнопку увеличения размера фото
+biggerSizeButton.addEventListener('click', decreaseButtonValue);
+
+//Вешаем слушатель на кнопку уменьшения размера фото
+smallerSizeButton.addEventListener('click', increaseButtonValue);
+
+export { scaleValueReset, imageUploadPreview };
