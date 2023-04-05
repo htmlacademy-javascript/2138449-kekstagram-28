3a064a3938e108ad19a6bf2f1d8d7fbbeec7e58e
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-04-05 12:20:04 +0300

    удалил моки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-import './big-picture.js';
+import './thumbnail.js';
 import './form.js';
-import './validation.js';
+import { setUserFormSubmit } from './validation.js';
 import './editor.js';
 import './editor-effects.js';
+import { showAlert } from './util.js';
+import { getData } from './api.js';
+import { renderPhotos } from './big-picture.js';
+import { closeEditor } from './form.js';
+
+const PHOTOS_COUNT = 25;
+
+getData()
+  .then((pictures) => {
+    renderPhotos(pictures.slice(0, PHOTOS_COUNT));
+  })
+  .catch((err) => {
+    showAlert(err.message);
+  });
+
+setUserFormSubmit(closeEditor);
+
